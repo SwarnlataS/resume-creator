263ebd2cfca7ccc26a99b211dce9592a5e244096
ganesh.c3110 <ganesh.c3110@gmail.com>
2018-02-25 23:04:54 +0530

    outer structure

---
+++ src/components/logo-header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div data-component="logo-header">
+        <header id="header">
+        <h1 class="heading">RESUMIZER</h1>
+        <h3 class="subheading">Free Online resume creator</h3>
+        <div class="signUpDiv">
+          <a href="#LoginModal" class="waves-effect waves-light btn modal-trigger">Sign In</a>
+          <a href="#NewSignInModal" class="waves-effect waves-light btn modal-trigger">New Sign In</a>
+        </div>
+      </header>
+    </div>
+</template>
+<script>
+</script>
+<style lang="sass">
+  
+</style>
